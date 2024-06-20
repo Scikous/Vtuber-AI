4a96b7ed909a9ebdb4442bcffc387f3fc0555212
Scikous <95752605+Scikous@users.noreply.github.com>
2024-06-20 21:15:04 +0300

    Refactored code, attempted response speed-up v0.1

--- LLM/model_utils.py
+++ LLM/model_utils.py
@@ -1,158 +1,269 @@
+# import json
+# import numpy as np
+# import re
+# from peft import PeftModel, PeftConfig
+# from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, DataCollatorForLanguageModeling, TrainingArguments, pipeline
+# # import datasets
+# # from difflib import SequenceMatcher
+# # responses are shorter or longer to create a more natural way of responding
+# def get_rand_token_len(min_tokens=15, max_tokens=100, input_len=0):
+#     """
+#     WIP
+#     given an input (STT/Comment), the potential response length should have a higher chance of being longer.  
+#     """
+#     # Adjust max tokens based on input length to avoid cutting off mid-thought
+#     adjusted_max_tokens = max(min_tokens, max_tokens - input_len)
+#     print(adjusted_max_tokens)
+#     tokens = np.arange(min_tokens, adjusted_max_tokens)
+#     token_weights = np.linspace(
+#         start=1.0, stop=0.05, num=adjusted_max_tokens-min_tokens)
+#     token_weights /= np.sum(token_weights)
+#     token_len = np.random.choice(tokens, p=token_weights)
+#     # print("TOKEENSS", token_len, tokens)
+#     return token_len
+
+# def model_loader(base_model_name="TheBloke/CapybaraHermes-2.5-Mistral-7B-GPTQ", custom_model_name=""):
+#     model = AutoModelForCausalLM.from_pretrained(base_model_name,
+#                                                 device_map="auto",
+#                                                 trust_remote_code=False,
+#                                                 # offload="LLM/offload",
+#                                                 revision="main")
+
+#     if custom_model_name:
+#         print(custom_model_name)
+#         config = PeftConfig.from_pretrained(custom_model_name)
+#         model = PeftModel.from_pretrained(
+#             model, custom_model_name, offload_folder="LLM/offload")
+#         # config.init_lora_weights = False
+#         # model.add_adapter(peft_config=config)
+#         # model.enable_adapters()
+
+#     model.eval()
+#     # load tokenizer
+#     tokenizer = AutoTokenizer.from_pretrained(base_model_name, use_fast=True)
+#     return model, tokenizer
+
+
+# def character_loader(character_info_json=""):
+#     if character_info_json:
+#         with open(character_info_json, 'r') as character:
+#             character_info = json.load(character)
+#             instructions, user_name, character_name = character_info[
+#                 "instructions"], character_info["user_name"], character_info["character_name"]
+#     else:
+#         instructions, user_name, character_name = "", "user", "assistant"
+#     return instructions, user_name, character_name
+
+# #remove words after last sentence stopper (., ?, !)
+# def sentence_reducer(output_clean):
+#     # Find the last occurrence of a sentence stopper
+#     match = re.search(r'[.!?](?!.*[.!?])', output_clean)
+#     if match:
+#         # Position of the last sentence stopper
+#         pos = match.end()
+#         #print("I AM EHEREREE"*10, pos)
+#         # Truncate the text at the position after the last stopper
+#         output_clean = output_clean[:pos].strip()
+#     return output_clean
+
+# class VtuberLLM():
+#     def __init__(self, model, tokenizer, character_name):
+#         self.model = model
+#         self.tokenizer = tokenizer
+#         self.character_name = character_name
+
+#     # generates character's response to a given input (TTS/Comment)
+#     def dialogue_generator(self, comment, PromptTemplate):
+#         # generated text may end prematurely, this should help avoid that
+#         def is_incomplete_sentence(text):
+#             # reply will end on not ., !, ? clean it beforehand then check
+#             return text.strip()[-1] not in {'.', '!', '?'}
+
+#         max_attempts = 7  # Set a limit to avoid infinite loops
+#         # continuation_prompt = "Continue from where the text within context left off. It is IMPERATIVE that you do NOT repeat anything already mentioned in the context. YOU simply just continue the previous without copying it."
+
+#         prompt = PromptTemplate(user_str=comment)
+#         comment_tokenized = self.tokenizer(comment, return_tensors="pt")
+#         inputs = self.tokenizer(prompt, return_tensors="pt")
+
+#         generated_text = ""
+#         for attempt in range(max_attempts):
+#             max_new_tokens = get_rand_token_len(
+#                 input_len=len(comment_tokenized["input_ids"][0]))
+#             results = self.model.generate(input_ids=inputs["input_ids"].to(
+#                 "cuda"), max_new_tokens=max_new_tokens, top_p=0.8, top_k=50, temperature=1.1, repetition_penalty=1.2, do_sample=True)
+#             output = self.tokenizer.batch_decode(results, skip_special_tokens=True)[0]
+#             # print(output)
+#             # only add new unique responses to final output
+#             print(f"{'#'*30}\n{output}\n{'#'*30}")
+#             output_clean = self.character_reply_cleaner(output).lower()
+#             print(f"{'#'*30}\n{output_clean}\n{'#'*30}")
+#             generated_text = output_clean
+#             # if attempt == 0:
+#             #     generated_text = output_clean
+#             #     gen_texts.append(generated_text)
+#             # else:
+#             #     similarity = SequenceMatcher(None, gen_texts[-1], output_clean).ratio()
+#             #     if similarity >= 0.4:
+#             #         new_text_incomplete = is_incomplete_sentence(output_clean)
+#             #         if not new_text_incomplete:
+#             #             gen_texts[-1] = output_clean
+#             #             generated_text = " ".join(gen_texts)
+#             #             print("*"*30,"\n\n",generated_text, "\n\n", "*"*30)
+#             #     else:
+#             #         generated_text = generated_text + ' ' + output_clean #add space after each generated response #character_reply_cleaner(copy.copy(output)).lower() #gen-text only needs actual response(s)
+#             #         gen_texts.append(output_clean)
+#             # print("WHEHEEE"*15, generated_text)
+#             if not is_incomplete_sentence(generated_text) or attempt == max_attempts:
+#                 break  # Break if the sentence is complete or max attempts reached
+
+#             # # Prepare for continuation
+#             # prompt = PromptTemplate(user_str=continuation_prompt, context_str=generated_text)
+#             # #print(prompt)
+#             # inputs = tokenizer(prompt, return_tensors="pt")
+#         # print(f"\n\n\nDKSDOSOJ{generated_text}\n\n\n")
+
+#         print("Text generation finished")
+#         return generated_text
+
+
+#     def character_reply_cleaner(self, reply):
+#         character_name = self.character_name+'\n'#"john\n"
+#         character_index = reply.find(character_name)
+
+#         if character_index != -1:
+#             reply = reply[character_index + len(character_name):]
+#         else:
+#             print("Womp womp", reply)
+#         # print("w"*30, '\n\n', reply, '\n\n',"w"*30, '\n\n')
+
+#         #########################################################
+#         # try:
+#         #     pattern = r"(?<=<\|im_start\|> John\n)\s*(.+?)(?=\<\/|\<|im_end\||$)"
+
+#         #     match = re.search(pattern, reply, re.DOTALL)
+#         #     clean_reply = match.group(0).strip() if match else "Womp Womp"
+#         #     if clean_reply == "Womp Womp":
+#         #         raise ValueError(" Womp Womp")
+#         #     print("hmmm")
+#         # except ValueError:  # not sure if getting triggered ever.
+#         #     pattern = r"(?<=<\|im_start\|> John\n)\s*(.+?)(?:\n|$)"
+#         #     match = re.search(pattern, reply, re.DOTALL)
+#         #     clean_reply = match.group(1).strip() if match else "Womp Womp sequel"
+#         #     print('ffff', clean_reply, 'ffffff')
+#         ##########################################################
+#         reply = sentence_reducer(reply)
+#         return reply
+
+
+
+
 import json
 import numpy as np
 import re
 from peft import PeftModel, PeftConfig
-from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, DataCollatorForLanguageModeling, TrainingArguments, pipeline
-# import datasets
-# from difflib import SequenceMatcher
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-# responses are shorter or longer to create a more natural way of responding
-def get_rand_token_len(min_tokens=15, max_tokens=100, input_len=0):
-    """
-    WIP
-    given an input (STT/Comment), the potential response length should have a higher chance of being longer.  
-    """
-    # Adjust max tokens based on input length to avoid cutting off mid-thought
-    adjusted_max_tokens = max(min_tokens, max_tokens - input_len)
-    print(adjusted_max_tokens)
-    tokens = np.arange(min_tokens, adjusted_max_tokens)
-    token_weights = np.linspace(
-        start=1.0, stop=0.05, num=adjusted_max_tokens-min_tokens)
-    token_weights /= np.sum(token_weights)
-    token_len = np.random.choice(tokens, p=token_weights)
-    # print("TOKEENSS", token_len, tokens)
-    return token_len
+class LLMUtils:
+    @staticmethod
+    def get_rand_token_len(min_tokens=15, max_tokens=100, input_len=0):
+        """
+        Given an input (STT/Comment), the potential response length should have a higher chance of being longer.
+        """
+        # Adjust max tokens based on input length to avoid cutting off mid-thought
+        adjusted_max_tokens = max(min_tokens, max_tokens - input_len)
+        print(adjusted_max_tokens)
+        tokens = np.arange(min_tokens, adjusted_max_tokens)
+        token_weights = np.linspace(
+            start=1.0, stop=0.05, num=adjusted_max_tokens - min_tokens)
+        token_weights /= np.sum(token_weights)
+        token_len = np.random.choice(tokens, p=token_weights)
+        return token_len
 
+    @staticmethod
+    def sentence_reducer(output_clean):
+        """
+        Remove words after the last sentence stopper (., ?, !)
+        """
+        match = re.search(r'[.!?](?!.*[.!?])', output_clean)
+        if match:
+            pos = match.end()
+            output_clean = output_clean[:pos].strip()
+        return output_clean
+    
+    @staticmethod
+    def load_model(base_model_name="TheBloke/CapybaraHermes-2.5-Mistral-7B-GPTQ", custom_model_name=""):
+        model = AutoModelForCausalLM.from_pretrained(base_model_name,
+                                                     device_map="auto",
+                                                     trust_remote_code=False,
+                                                     revision="main")
 
-def sentence_reducer(output_clean):
-    # Find the last occurrence of a sentence stopper
-    match = re.search(r'[.!?](?!.*[.!?])', output_clean)
-    if match:
-        # Position of the last sentence stopper
-        pos = match.end()
-        #print("I AM EHEREREE"*10, pos)
-        # Truncate the text at the position after the last stopper
-        output_clean = output_clean[:pos].strip()
-    return output_clean
+        if custom_model_name:
+            print(custom_model_name)
+            config = PeftConfig.from_pretrained(custom_model_name)
+            model = PeftModel.from_pretrained(
+                model, custom_model_name, offload_folder="LLM/offload")
 
-# generates character's response to a given input (TTS/Comment)
+        model.eval()
+        tokenizer = AutoTokenizer.from_pretrained(base_model_name, use_fast=True)
+        return model, tokenizer
+    
+    @staticmethod
+    def load_character(character_info_json=""):
+        if character_info_json:
+            with open(character_info_json, 'r') as character:
+                character_info = json.load(character)
+                instructions = character_info["instructions"]
+                user_name = character_info["user_name"]
+                character_name = character_info["character_name"]
+        else:
+            instructions, user_name, character_name = "", "user", "assistant"
+        return instructions, user_name, character_name
 
+class VtuberLLM:
+    def __init__(self, model, tokenizer, character_name):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.character_name = character_name
 
-def dialogue_generator(model, tokenizer, comment, PromptTemplate):
+    def dialogue_generator(self, comment, PromptTemplate):
+        """
+        Generates character's response to a given input (TTS/Comment)
+        """
+        def is_incomplete_sentence(text):
+            return text.strip()[-1] not in {'.', '!', '?'}
 
-    # generated text may end prematurely, this should help avoid that
-    def is_incomplete_sentence(text):
-        # reply will end on not ., !, ? clean it beforehand then check
-        return text.strip()[-1] not in {'.', '!', '?'}
+        max_attempts = 7
+        prompt = PromptTemplate(user_str=comment)
+        comment_tokenized = self.tokenizer(comment, return_tensors="pt")
+        inputs = self.tokenizer(prompt, return_tensors="pt")
 
-    max_attempts = 7  # Set a limit to avoid infinite loops
-    # continuation_prompt = "Continue from where the text within context left off. It is IMPERATIVE that you do NOT repeat anything already mentioned in the context. YOU simply just continue the previous without copying it."
+        generated_text = ""
+        for attempt in range(max_attempts):
+            max_new_tokens = LLMUtils.get_rand_token_len(input_len=len(comment_tokenized["input_ids"][0]))
+            results = self.model.generate(input_ids=inputs["input_ids"].to("cuda"),
+                                          max_new_tokens=max_new_tokens,
+                                          top_p=0.8, top_k=50, temperature=1.1,
+                                          repetition_penalty=1.2, do_sample=True)
+            output = self.tokenizer.batch_decode(results, skip_special_tokens=True)[0]
+            print(f"{'#' * 30}\n{output}\n{'#' * 30}")
+            output_clean = self.character_reply_cleaner(output).lower()
+            print(f"{'#' * 30}\n{output_clean}\n{'#' * 30}")
+            generated_text = output_clean
+            if not is_incomplete_sentence(generated_text) or attempt == max_attempts:
+                break
 
-    prompt = PromptTemplate(user_str=comment)
-    comment_tokenized = tokenizer(comment, return_tensors="pt")
-    inputs = tokenizer(prompt, return_tensors="pt")
+        print("Text generation finished")
+        return generated_text
 
-    generated_text = ""
-    # gen_texts = []
-    for attempt in range(max_attempts):
-        # print(f"WOMPWOM: {len(comment_tokenized['input_ids'][0])}")
-        max_new_tokens = get_rand_token_len(
-            input_len=len(comment_tokenized["input_ids"][0]))
-        results = model.generate(input_ids=inputs["input_ids"].to(
-            "cuda"), max_new_tokens=max_new_tokens, top_p=0.8, top_k=50, temperature=1.1, repetition_penalty=1.2, do_sample=True)
-        output = tokenizer.batch_decode(results, skip_special_tokens=True)[0]
-        # print(output)
-        # only add new unique responses to final output
-        print(f"{'#'*30}\n{output}\n{'#'*30}")
-        output_clean = character_reply_cleaner(output).lower()
-        print(f"{'#'*30}\n{output_clean}\n{'#'*30}")
-        generated_text = output_clean
-        # if attempt == 0:
-        #     generated_text = output_clean
-        #     gen_texts.append(generated_text)
-        # else:
-        #     similarity = SequenceMatcher(None, gen_texts[-1], output_clean).ratio()
-        #     if similarity >= 0.4:
-        #         new_text_incomplete = is_incomplete_sentence(output_clean)
-        #         if not new_text_incomplete:
-        #             gen_texts[-1] = output_clean
-        #             generated_text = " ".join(gen_texts)
-        #             print("*"*30,"\n\n",generated_text, "\n\n", "*"*30)
-        #     else:
-        #         generated_text = generated_text + ' ' + output_clean #add space after each generated response #character_reply_cleaner(copy.copy(output)).lower() #gen-text only needs actual response(s)
-        #         gen_texts.append(output_clean)
-        # print("WHEHEEE"*15, generated_text)
-        if not is_incomplete_sentence(generated_text) or attempt == max_attempts:
-            break  # Break if the sentence is complete or max attempts reached
+    def character_reply_cleaner(self, reply):
+        character_name = self.character_name + '\n'
+        character_index = reply.find(character_name)
 
-        # # Prepare for continuation
-        # prompt = PromptTemplate(user_str=continuation_prompt, context_str=generated_text)
-        # #print(prompt)
-        # inputs = tokenizer(prompt, return_tensors="pt")
-    # print(f"\n\n\nDKSDOSOJ{generated_text}\n\n\n")
-
-    print("Text generation finished")
-    return generated_text
-
-
-def character_reply_cleaner(reply):
-    character = "John\n"
-    character_index = reply.find(character)
-
-    if character_index != -1:
-        reply = reply[character_index + len("John\n"):]
-    else:
-        print("Womp womp", reply)
-    # print("w"*30, '\n\n', reply, '\n\n',"w"*30, '\n\n')
-
-    #########################################################
-    # try:
-    #     pattern = r"(?<=<\|im_start\|> John\n)\s*(.+?)(?=\<\/|\<|im_end\||$)"
-
-    #     match = re.search(pattern, reply, re.DOTALL)
-    #     clean_reply = match.group(0).strip() if match else "Womp Womp"
-    #     if clean_reply == "Womp Womp":
-    #         raise ValueError(" Womp Womp")
-    #     print("hmmm")
-    # except ValueError:  # not sure if getting triggered ever.
-    #     pattern = r"(?<=<\|im_start\|> John\n)\s*(.+?)(?:\n|$)"
-    #     match = re.search(pattern, reply, re.DOTALL)
-    #     clean_reply = match.group(1).strip() if match else "Womp Womp sequel"
-    #     print('ffff', clean_reply, 'ffffff')
-    ##########################################################
-    reply = sentence_reducer(reply)
-    return reply
-
-
-def model_loader(base_model_name="TheBloke/CapybaraHermes-2.5-Mistral-7B-GPTQ", custom_model_name=""):
-    model = AutoModelForCausalLM.from_pretrained(base_model_name,
-                                                 device_map="auto",
-                                                 trust_remote_code=False,
-                                                 # offload="LLM/offload",
-                                                 revision="main")
-
-    if custom_model_name:
-        print(custom_model_name)
-        config = PeftConfig.from_pretrained(custom_model_name)
-        model = PeftModel.from_pretrained(
-            model, custom_model_name, offload_folder="LLM/offload")
-        # config.init_lora_weights = False
-        # model.add_adapter(peft_config=config)
-        # model.enable_adapters()
-
-    model.eval()
-    # load tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(base_model_name, use_fast=True)
-    return model, tokenizer
-
-
-def character_loader(character_info_json=""):
-    if character_info_json:
-        with open(character_info_json, 'r') as character:
-            character_info = json.load(character)
-            instructions, user_name, character_name = character_info[
-                "instructions"], character_info["user_name"], character_info["character_name"]
-    else:
-        instructions, user_name, character_name = "", "user", "assistant"
-    return instructions, user_name, character_name
-
+        if character_index != -1:
+            reply = reply[character_index + len(character_name):]
+        else:
+            print("Womp womp", reply)
+            
+        reply = LLMUtils.sentence_reducer(reply)
+        return reply
--- voiceAI/STT.py
+++ voiceAI/STT.py
@@ -8,10 +8,7 @@ def STT(callback):
             print("Speak something...")
             recognizer.energy_threshold = 2800
             #print("Listening...")
-            try:
-                audio_data = recognizer.listen(source, timeout=5)
-            except Exception as e:
-                print("har har har", e)
+            audio_data = recognizer.listen(source, phrase_time_limit=27)
             #print("Processing...")
 
         try:
@@ -27,10 +24,28 @@ def STT(callback):
 
     recognize_speech()
 
+class SpeechToText:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()#recognizer
+
+    def recognize_speech(self):
+        with sr.Microphone() as source:
+            audio_data = self.recognizer.listen(source, phrase_time_limit=5)
+        text = ""
+        try:
+            text = self.recognizer.recognize_whisper(audio_data, language='english')
+        except sr.UnknownValueError:
+            print("Sorry, could not understand audio.")
+        except sr.RequestError as e:
+            print("Error: Could not request results from Whisper service;")
+        return text
+
     #threading.Thread(target=recognize_speech, daemon=True).start()
 
 if __name__ == "__main__":
-    def print_callback(text):
-        print("Callback:", text)
+    stt = SpeechToText()
+    t = stt.recognize_speech()
+    # def print_callback(text):
+    #     print("Callback:", text)
     
-    STT(print_callback)
+    # STT(print_callback)
