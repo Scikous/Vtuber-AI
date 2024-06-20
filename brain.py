17770def41fe23a01b50bb3b5c55c26cf6d73736
Scikous <rozeusker@gmail.com>
2024-06-20 21:15:04 +0300

    Refactored code, attempted response speed-up v0.1

--- brain.py
+++ brain.py
@@ -1,7 +1,7 @@
-from LLM.model_utils import model_loader, dialogue_generator, character_loader
+from LLM.model_utils import VtuberLLM, LLMUtils
 from voiceAI.TTS import send_tts_request
 from LLM.llm_templates import PromptTemplate as pt   # PromptTemplate as pt
-from voiceAI.STT import STT
+from voiceAI.STT import STT, SpeechToText
 import time
 from queue import Queue
 import logging
@@ -23,7 +23,7 @@ def stt_worker():
     while True:
         STT(stt_callback)
         # Add a short sleep to avoid tight loop
-        time.sleep(5)
+        time.sleep(2)
 
 
 def loop_function():
@@ -37,31 +37,106 @@ def loop_function():
             # print(speech_queue.queue)
             # logging.debug(f"Speech queue: {speech_queue.queue}")
             comment = speech
-            output = dialogue_generator(
-                model, tokenizer, comment, PromptTemplate.capybaraChatML)
+            output = Character.dialogue_generator(comment, PromptTemplate.capybaraChatML)
             print("THEOUTPUT IS", output)
             # clean_reply = character_reply_cleaner(output).lower()
             send_tts_request(output)
-            time.sleep(7)
+            # time.sleep(7)
         except ValueError:
             pass
         except Exception as e:
             logging.error(f"Unexpected error: {e}")
 
+######################################################################################################################
+#WIP
+# from threading import Thread
+
+# class STTWorker(Thread):
+#     def __init__(self, speech_queue):
+#         Thread.__init__(self)
+#         # self.recognizer = recognizer
+#         self.speech_queue = speech_queue
+#         # self.daemon = True
+
+
+#     def run(self):
+#         stt = SpeechToText()
+#         while True:
+#             print("hello from STT")
+#             speech = stt.recognize_speech()
+#             print("hello", speech)
+#             self.speech_queue.put(speech)
+#             print(f"STTWorker recognized: {speech}")
+#             time.sleep(0.1)
+
+# class TTSWorker(Thread):
+#     def __init__(self, tts_queue):
+#         Thread.__init__(self)
+#         # self.tts_engine = tts_engine
+#         self.tts_queue = tts_queue
+#         self.daemon = True
+
+
+
+#     def run(self):
+#         # tts = TextToSpeech(self.tts_engine)
+#         print("hello from TTS")
+#         while True:
+#             if not self.tts_queue.empty():
+#                 text_to_speak = self.tts_queue.get()
+#                 send_tts_request(text_to_speak)
+#                 # tts.speak_text(text_to_speak)
+#                 print(f"TTSWorker spoke: {text_to_speak}")
+#             time.sleep(0.1)
+
+# class DialogueWorker(Thread):
+#     def __init__(self, vtuber_llm, speech_queue, tts_queue, prompt_template):
+#         Thread.__init__(self)
+#         # self.daemon = True
+#         self.vtuber_llm = vtuber_llm
+#         self.speech_queue = speech_queue
+#         self.tts_queue = tts_queue
+#         self.prompt_template = prompt_template
+
+
+#     def run(self):
+#         print("hello from dialogue")
+#         while True:
+#             if not self.speech_queue.empty():
+#                 comment = self.speech_queue.get()
+#                 response = self.vtuber_llm.dialogue_generator(comment, self.prompt_template)
+#                 self.tts_queue.put(response)
+#                 print(f"DialogueWorker generated response: {response}")
+#             time.sleep(0.1)
+
+######################################################################################################################
 
 if __name__ == "__main__":
     custom_model = "LLM/unnamedSICUACCT"
-    model, tokenizer = model_loader(custom_model_name=custom_model)
+    model, tokenizer = LLMUtils.load_model(custom_model_name=custom_model)
     
     character_info_json = "LLM/characters/character.json"
-    character_info = character_loader(character_info_json)
+    instructions, user_name, character_name = LLMUtils.load_character(character_info_json)
     
     # with open("LLM/characters/character.txt", "r") as f:
     #     character_info = f.readline()
-    instructions_string = f"""{character_info}"""
-    PromptTemplate = pt(
-        instructions_str=instructions_string, character_name="John")
+    instructions_string = f"""{instructions}"""
+    PromptTemplate = pt(instructions_string, user_name, character_name)
+    Character = VtuberLLM(model, tokenizer, character_name)  
 
     speech_queue = Queue(maxsize=5)
+    tts_queue = Queue()
+
+    # stt_worker = STTWorker(speech_queue)
+    # dialogue_worker = DialogueWorker(Character, speech_queue, tts_queue, PromptTemplate.capybaraChatML)
+    # tts_worker = TTSWorker(tts_queue)
+
+    # stt_worker.start()
+    # dialogue_worker.start()
+    # tts_worker.start()
+
+    # stt_worker.join()
+    # dialogue_worker.join()
+    # tts_worker.join()
 
     loop_function()
