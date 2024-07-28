dea0a82cb31099d426680b13da5677503c082b18
Scikous <95752605+Scikous@users.noreply.github.com>
2024-07-28 11:13:13 +0300

    fixed twitch flask server not shutting down correctly, general code keeping - added comments to clarify different sections of code - twitch credentials are handled and accessed directly by/from TwitchAuth class - threads handling livechat messages now lock themselves to avoid possible conflict -- experimental unknown real world results - removed unnecessary code

--- livechatAPI/livechat.py
+++ livechatAPI/livechat.py
@@ -1,263 +1,68 @@
-# from youtube import YTLive, YTTools
-# from livechat_utils import ChatPicker 
-# from twitch import TwitchAuth, TwitchTools, Bot, twitch_chat_msgs
-# import threading
-
-# class Livechats():
-#     def __init__(self, Youtube=None, Twitch=None, Kick=None):
-#         self.Youtube = Youtube
-#         self.Twitch = Twitch
-#         self.Kick = Kick
-        
-#     def setup_twitch_bot():
-#         CHANNEL = 'scikous'
-#         BOT_NICK = 'Botty'
-
-#         TWTools = TwitchTools()
-#         CLIENT_ID, CLIENT_SECRET = TWTools.twitch_auth_loader("livechatAPI/credentials/twitch.json")
-#         TW_Auth = TwitchAuth(CLIENT_ID, CLIENT_SECRET)
-
-#         # # Replace with your Twitch token and channel
-#         TOKEN = TW_Auth.auth_access_token()
-#         bot = Bot(TOKEN,CLIENT_ID, BOT_NICK, CHANNEL)
-#         bot.run()
-# #basic setup to start fetching live chat messages from live stream
-# def youtube_setup(video_id):
-#     api_key = YTTools.api_key_loader('livechatAPI/credentials/youtube.json')
-#     live_chat_id = YTTools.get_live_chat_id(video_id=video_id)
-
-#     YTLIVE = YTLive(api_key=api_key, live_chat_id=live_chat_id)
-#     return YTLIVE
-
-
-
-# async def livechats_setup(fetch_youtube=False, fetch_twitch=False, fetch_kick=False):
-#     if fetch_youtube:
-#         YTLIVE = youtube_setup(video_id="oQYdDy86eoY")
-#     if fetch_twitch:
-#         twitch_thread = threading.Thread(target=run_twitch_bot, daemon=True)
-#         twitch_thread.start()
-#     if fetch_kick:
-#         pass
-#     # print(twitch_chat_msgs)
-    
-# def fetch_chat_msgs()
-#     yt_messages, next_page_token = YTLIVE.get_live_chat_messages()
-#     kick = []
-#     picker = ChatPicker(yt_messages, twitch_chat_msgs, kick)
-
-#     message = picker.pick_rand_message()
-#     print(message)
-#     if yt_messages:
-#         return yt_messages[0][1], next_page_token
-#     else:
-#         return None, None
-
-# # import asyncio
-# # msg = asyncio.run(fetch_chat_msgs())
-# # print(msg[0])
-
-
-
-# ##################################
-# # {
-# #     "yt": (
-# #         "user": "msg",
-# #         "user2": "msg2",
-# #     )
-# #     "tw": (
-# #         {
-# #             "user": "msg",
-# #             "user2": "msg2",
-# #         }
-# #     )
-# #     "ki": (
-# #         "user": "msg",
-# #         "user2": "msg2",
-# #     )
-# # }
-
-# # {
-# #     "yt": (
-# #         (user, msg),
-# #         (user2, msg2)
-# #     ),
-# #     "tw": (
-# #         (user, msg),
-# #         (user2, msg2)
-# #     ),
-# #     "ki": (
-# #         (user, msg),
-# #         (user2, msg2)
-# #     )
-# # }
-
-# #########
-#     #     csv_reader = csv.reader(file)
-#     #     messages = tuple((tuple(row) for row in csv_reader))
-#     # return messages[-num_messages:]
-
-
-# # file = 'livechatAPI/data/kick_chat.csv'
-# # # messages_to_csv(file, (("john", "msg0"), ("bob","msg")))
-# # messages = read_messages_csv(file)
-# # print(messages)
-
-
-# # ############################
-# # from twitch import TwitchTools, TwitchAuth, Bot
-# # import threading
-# # TWTools = TwitchTools()
-# # CLIENT_ID, CLIENT_SECRET = TWTools.twitch_auth_loader("livechatAPI/credentials/twitch.json")
-# # CHANNEL = 'scikous'
-# # BOT_NICK = 'Botty'
-
-# # TW_Auth = TwitchAuth(CLIENT_ID, CLIENT_SECRET)
-
-
-# # # Replace with your Twitch token and channel
-# # TOKEN = TW_Auth.auth_access_token()
-# # bot = Bot(TOKEN,CLIENT_ID, BOT_NICK, CHANNEL)
-# # twitch_thread = threading.Thread(target=bot.run, daemon=True)
-# # twitch_thread.start()
-
-
-
-
-
-
-# import threading
-# from youtube import YTLive, YTTools
-# from livechat_utils import ChatPicker 
-# from twitch import TwitchAuth, TwitchTools, Bot, twitch_chat_msgs
-# import time
-
-
-
-# def setup_twitch():
-#     CHANNEL = 'scikous'
-#     BOT_NICK = 'Botty'
-
-#     TWTools = TwitchTools()
-#     CLIENT_ID, CLIENT_SECRET = TWTools.twitch_auth_loader("livechatAPI/credentials/twitch.json")
-#     TW_Auth = TwitchAuth(CLIENT_ID, CLIENT_SECRET)
-
-#     # Replace with your Twitch token and channel
-#     TOKEN = TW_Auth.auth_access_token()
-#     bot = Bot(TOKEN, CLIENT_ID, BOT_NICK, CHANNEL)
-#     return bot
-
-
-
-# def setup_youtube():
-#     api_key = YTTools.api_key_loader('livechatAPI/credentials/youtube.json')
-#     youtube = YTLive(api_key=api_key, video_id="oQYdDy86eoY")
-#     return youtube
-
-# bot = setup_twitch()
-# twitch_thread = threading.Thread(target=bot.run, daemon=True)
-# twitch_thread.start()
-# youtube = setup_youtube()
-
-# def fetch_chat_msgs():
-#     yt_messages = []
-#     if youtube:
-#         yt_messages, next_page_token = youtube.get_live_chat_messages(next_page_token)
-#     else:
-#         next_page_token = None
-    
-#     kick = []  # Placeholder for Kick messages
-#     picker = ChatPicker(yt_messages, twitch_chat_msgs, kick)
-
-#     message = picker.pick_rand_message()
-#     print(message)
-#     return message
-# while True:
-#     fetch_chat_msgs()
-#     time.sleep(20)
-
-# # ##kick api
-# from kick_chat import client
-
-# p = client.Client(username="scikous")
-# print(p.listen())
-
-# kick_thread= threading.Thread(target=p.listen, daemon=True)
-# kick_thread.start()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import threading
 from youtube import YTLive, yt_messages
 from livechat_utils import ChatPicker 
-from twitch import TwitchAuth, TwitchTools, Bot, twitch_chat_msgs
+from twitch import TwitchAuth, Bot, twitch_chat_msgs
 import time
 
 kick = []  # Placeholder for Kick messages
-class LiveChatSetup:
-    def __init__(self, fetch_twitch=False, fetch_youtube=False):
-        self.fetch_twitch = fetch_twitch
-        self.fetch_youtube = fetch_youtube
+
+#
+class LiveChatController:
+    def __init__(self, fetch_twitch=False, fetch_youtube=False, fetch_kick=False):
         self.twitch_bot = None
         self.youtube = None
         self.next_page_token = None
-        self.picker = ChatPicker(yt_messages, twitch_chat_msgs, kick)
+        chat_sources = []
 
-        if self.fetch_twitch:
+        if fetch_twitch:
+            chat_sources.append(twitch_chat_msgs)
             self.setup_twitch()
 
-        if self.fetch_youtube:
+        if fetch_youtube:
+            chat_sources.append(yt_messages)
             self.setup_youtube()
+        
+        if fetch_kick:
+            chat_sources.append(kick)
+            self.setup_kick()
 
+        #only desired chats should be included in the random message picking
+        self.picker = ChatPicker(*chat_sources)
+
+    #get token and start twitch bot on a separate thread for livechat messages
     def setup_twitch(self):
-        CHANNEL = 'scikous'
-        BOT_NICK = 'Botty'
-
-        TWTools = TwitchTools()
-        CLIENT_ID, CLIENT_SECRET = TWTools.twitch_auth_loader("livechatAPI/credentials/twitch.json")
-        TW_Auth = TwitchAuth(CLIENT_ID, CLIENT_SECRET)
-
-        # Replace with your Twitch token and channel
-        TOKEN = TW_Auth.auth_access_token()
-        self.twitch_bot = Bot(TOKEN, CLIENT_ID, BOT_NICK, CHANNEL)
+        TW_Auth = TwitchAuth()
+        CHANNEL, BOT_NICK, CLIENT_ID, TOKEN = TW_Auth.CHANNEL, TW_Auth.BOT_NICK, TW_Auth.CLIENT_ID, TW_Auth.TOKEN, 
+        if not TOKEN:#get new TOKEN if no token or token has expired
+            TOKEN = TW_Auth.auth_access_token()
+        self.twitch_bot = Bot(CHANNEL, BOT_NICK, CLIENT_ID, TOKEN)
         twitch_thread = threading.Thread(target=self.twitch_bot.run, daemon=True)
         twitch_thread.start()
+    #WIP
+    def setup_kick(self):
+        pass
 
+    #get token and prepare for fetching youtube livechat messages
     def setup_youtube(self):
         youtube_creds_file = 'livechatAPI/credentials/youtube.json'
         self.youtube = YTLive(youtube_creds_file)
 
+    #fetch a random message from 
     async def fetch_chat_message(self):
         if self.youtube:
             self.next_page_token = self.youtube.get_live_chat_messages(next_page_token=self.next_page_token)
         message = self.picker.pick_rand_message()
-        # print(message)
+        # print("PICKED MESSAGE:", message)
         return message
 
 # Example usage:
 if __name__ == "__main__":
     import asyncio
-    fetch_twitch = False
-    fetch_youtube = True
+    fetch_twitch = True
+    fetch_youtube = False
 
-    live_chat_setup = LiveChatSetup(fetch_twitch=fetch_twitch, fetch_youtube=fetch_youtube)
+    live_chat_setup = LiveChatController(fetch_twitch=fetch_twitch, fetch_youtube=fetch_youtube)
 
     while True:
         asyncio.run(live_chat_setup.fetch_chat_message())
-        time.sleep(10)  # Adjust the interval as needed
+        time.sleep(15.5)  # Adjust the interval as needed
--- livechatAPI/livechat_utils.py
+++ livechatAPI/livechat_utils.py
@@ -1,39 +1,55 @@
 import random
 from collections import deque
 import csv
+import threading
 
+lock = threading.Lock() #may or may not be useful in avoiding messing with different livechats at wrong times
 
-
+#save livechat message(s) for convenient data
 def write_messages_csv(file_path, messages):
     '''
-    intented for writing tuples of data (<user name>, <message>)
+    intented for writing tuples of chat messages -> (<user name>, <message>)
     can technically be any format
     '''
-    with open(file_path, mode='a', newline='', encoding='utf-8') as file:
+    global lock
+
+    with open(file_path, mode='a', newline='\n', encoding='utf-8') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerows(messages)
+        with lock:
+            if isinstance(messages, list):
+                csv_writer.writerows(messages)
+            else:#assumes single message -- for twitch and kick
+                csv_writer.writerow(messages)    
 
-
+#fetch last 10 messages from csv
 def read_messages_csv(file_path, num_messages=10):
     '''
-    intented for reading tuples of data (<user name>, <message>)
+    intented for return tuples of chat messages -> (<user name>, <message>)
     can technically be any format
-    by default returns the latest 10 tuples of data as a list of tuples
+    by default returns the latest 10 chat messages as a list of tuples -> [(<user name>, <message>), ...]
     '''
+
     with open(file_path, mode='r', newline='', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         messages = deque(csv_reader, maxlen=num_messages)
     return [tuple(row) for row in messages]
 
+#each livechat should keep to a set limit avoiding favoritism
+def append_livechat_message(chat_messages: list, user_msg: tuple):
+    """
+    adds the latest message to the running list of messages while maintaining a set max size (default is 10)
+    for twitch and kick livechats
+    """
 
-def append_message(chat_messages: list, user_msg: tuple):
+    global lock
     MAX_MESSAGES = 10
-    if len(chat_messages) < MAX_MESSAGES:
-        chat_messages.append(user_msg)
-    else:
-        chat_messages.pop(0)
+    with lock:
+        # print(chat_messages)
+        if len(chat_messages) >= MAX_MESSAGES:
+            chat_messages.pop(0)
         chat_messages.append(user_msg)
 
+#takes lists of chat messages and allows for selecting a random message between them 
 class ChatPicker:
     def __init__(self, *lists):
         self.lists = lists
@@ -70,9 +86,8 @@ class ChatPicker:
         for i, prob in probabilities.items():
             cumulative_prob += prob
             if rand < cumulative_prob:
-                self.pick_counts[i] += 1  # Increment pick count for the chosen list
+                self.pick_counts[i] += 1  # Increment pick count for the chosen list -- lowers chances of being chosen again later
                 random_chat_message = random.choice(self.lists[i])
-                #remove chosen message from list
-                self.lists[i].remove(random_chat_message)
+                self.lists[i].remove(random_chat_message) #remove chosen message from list
                 # print(self.lists)
                 return random_chat_message
